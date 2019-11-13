fbd3013af4c4a20d2dcf00b44dbe3577478d96f0
Munoon <munoongg@gmail.com>
2019-11-13 21:27:18 +0200

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,242 @@
+const userName = document.getElementById('userName');
+const userBalance = document.getElementById('userBalance');
+const chooseUserRow = document.getElementById('chooseUserRow');
+const creditCardInput = document.getElementById('creditCardInput');
+const itemsToBuy = document.getElementById('itemsToBuy');
+const itemsToBuyList = document.getElementById('itemsToBuyList');
+const totalSum = document.getElementById('totalSum');
+const pincodeBox = document.querySelector('.pincode-box');
+const pincodeTitile = document.getElementById('pincodeTitile');
+const pincodeRow = document.getElementById('pincodeRow');
+let pincodeCircle = document.querySelector('.circle-loader');
+let pincodeCheckmark = document.querySelector('.checkmark');
+let pincodeCallback = function () {};
+const cancel = () => $(chooseUserRow).modal();
+let rowHideAble = false;
+let selectedUser, failedNote;
+
+$(() => {
+    $.ajaxSetup({cache: false});
+
+    $(document).ajaxError((event, jqXHR) => failNoty(jqXHR));
+
+    document.getElementById('pincodeButtons').addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.nodeName !== 'INPUT') {
+            return;
+        }
+
+        let value = e.target.value;
+        addNumber(value);
+    });
+
+    $.get('/ajax/bar/items?shown=true')
+        .done(response => {
+            response.forEach(item => {
+                let img = '';
+                if (item.image !== undefined && item.image !== '') {
+                    img = `<img src="${barImgUrl + item.image}" class="card-img-top" alt="${item.name}">`;
+                }
+                itemsToBuy.insertAdjacentHTML('beforeend', `
+                    <div class="col-sm-2">
+                        <div class="card" style="width: 9rem;" data-id="${item.id}" data-name="${item.name}" data-price="${item.price}">
+                            ${img}
+                            <div class="card-body">
+                                <h5 class="card-title text-center">${item.name}</h5>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            })
+        });
+
+    creditCardInput.addEventListener('input', e => {
+        let creditCard = creditCardInput.value;
+        if (creditCard.length !== 12) {
+            return;
+        }
+
+        $.ajax('/ajax/bar/get/' + creditCard)
+            .done(response => {
+                selectedUser = response;
+                userName.textContent = response.name + ' ' + response.surname;
+                userBalance.textContent = response.balance + ' грандиков';
+                itemsToBuyList.innerHTML = '';
+                totalSum.textContent = 'Всего: 0';
+                rowHideAble = true;
+                $(chooseUserRow).modal('hide');
+                rowHideAble = false;
+                creditCardInput.value = '';
+            });
+    });
+
+    itemsToBuy.addEventListener('click', e => {
+        let target = e.target.closest('.card');
+        if (target === null) {
+            return;
+        }
+
+        let price = target.dataset.price;
+
+        itemsToBuyList.insertAdjacentHTML('beforeend', `
+            <li class="list-group-item d-flex justify-content-between align-items-center" data-id="${target.dataset.id}" data-price="${price}">
+                ${target.dataset.name}
+                <span class="badge badge-primary badge-pill">${price}</span>
+            </li>
+        `);
+        updatePrice();
+    });
+
+    itemsToBuyList.addEventListener('click', e => {
+        let target = e.target.closest('li');
+        itemsToBuyList.removeChild(target);
+        updatePrice();
+    });
+
+    $(chooseUserRow).modal({ keyboard: false });
+    $(chooseUserRow).on('hide.bs.modal', e => rowHideAble);
+});
+
+function showPincode() {
+    pincodeCallback = buy;
+    $(pincodeRow).modal();
+}
+
+function buy(pinCode) {
+    let data = getBuyData();
+
+    pincodeBox.hidden = true;
+    pincodeCircle.hidden = false;
+
+    $.ajax({
+        url: `/ajax/bar?userId=${selectedUser.id}&pinCode=${pinCode}`,
+        method: 'POST',
+        data: JSON.stringify(data.items),
+        contentType: "application/json; charset=utf-8",
+        error: data => {
+            togglePinError();
+            setTimeout(() => {
+                pincodeBox.value = '';
+                pincodeBox.hidden = false;
+                pincodeCircle.hidden = true;
+                setDefaultPinLoader();
+            }, 1000);
+        }
+    }).done(response => {
+        togglePinComplete();
+        successNoty("Товарвы были успешны куплены!");
+        setTimeout(() => {
+            $(pincodeRow).modal('hide');
+            pincodeBox.value = '';
+            $(chooseUserRow).modal();
+            pincodeBox.hidden = false;
+            pincodeCircle.hidden = true;
+            setDefaultPinLoader();
+        }, 1000);
+    });
+}
+
+function updatePrice() {
+    let totalPrice = getBuyData().totalPrice;
+    totalSum.textContent = 'Всего: ' + totalPrice;
+    userBalance.style.color = selectedUser.balance - totalPrice < 0 ? 'red' : 'black';
+}
+
+function getBuyData() {
+    let totalPrice = 0;
+    let items = [];
+    itemsToBuyList.childNodes.forEach(el => {
+        if (el.nodeName !== 'LI') {
+            return;
+        }
+
+        totalPrice += +el.dataset.price;
+        items.push(+el.dataset.id);
+    });
+    return { totalPrice, items };
+}
+
+function closeNoty() {
+    if (failedNote) {
+        failedNote.close();
+        failedNote = undefined;
+    }
+}
+
+function successNoty(msg) {
+    closeNoty();
+    new Noty({
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + msg,
+        type: 'success',
+        layout: "bottomRight",
+        timeout: 1000
+    }).show();
+}
+
+function failNoty(jqXHR) {
+    closeNoty();
+    const errorInfo = JSON.parse(jqXHR.responseText);
+    failedNote = new Noty({
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + errorInfo.type + "<br>" + errorInfo.details.join("<br>"),
+        type: "error",
+        layout: "bottomRight"
+    }).show();
+}
+
+function getNormalText(text) {
+    if (text === undefined || text === null) {
+        return '';
+    }
+    return text;
+}
+
+function getDate(date) {
+    let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    let month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth();
+    return day + '.' + month + '.' + date.getFullYear();
+}
+
+function getTime(date) {
+    let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    return hours + ':' + minutes;
+}
+
+function checkForPincodeDone() {
+    if (pincodeBox.value.length === 4) {
+        pincodeCallback(pincodeBox.value);
+    }
+}
+
+function addNumber(num) {
+    pincodeBox.value = pincodeBox.value + num;
+    checkForPincodeDone();
+}
+
+function backspace() {
+    let value = pincodeBox.value;
+    pincodeBox.value = value.substring(0, value.length - 1);
+}
+
+function closePincode() {
+    $(pincodeRow).modal('hide');
+}
+
+function setPincodeTitle(title) {
+    pincodeTitile.textContent = title;
+}
+
+function togglePinComplete() {
+    $(pincodeCircle).removeClass('load-error').toggleClass('load-complete');
+    $(pincodeCheckmark).removeClass('error').addClass('draw').toggle();
+}
+
+function togglePinError() {
+    $(pincodeCircle).removeClass('load-complete').toggleClass('load-error');
+    $(pincodeCheckmark).removeClass('draw').addClass('error').toggle();
+}
+
+function setDefaultPinLoader() {
+    $(pincodeCircle).removeClass('load-error').removeClass('load-complete');
+    $(pincodeCheckmark).removeClass('error').addClass('draw').toggle();
+}
